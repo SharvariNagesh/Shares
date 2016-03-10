ac2df2d9525f43492138b5fa3bd2c5c45b91eacf
SharvariNagesh <sharvari.nagesh@gmail.com>
2016-03-10 11:11:22 +1100

    Spec to view an individual share

---
+++ spec/features/show_share_spec.rb
@@ -0,0 +1,40 @@
+require "spec_helper"
+require "rails_helper"
+
+describe "Vieweing details of a share" do
+	it "Viewing Individual Share detail" do
+		
+
+
+		share1 = Share.create(name: "Nestle",
+                      current_price: 5304,
+                      year_low: 4990,
+                      year_high: 7499,
+                      current_PE_ratio: 90.80,
+                      market_cap: 51143.23,
+                      book_value: 352.69,
+                      description: "Nestle is in the Food Processing sector. The current market capitalisation stands at Rs 51,143.23 crore.",
+                      price_to_book_value: 15.40)
+
+		share2 = Share.create(name: "Colgate Palmolive",
+                      current_price: 826,
+                      year_low: 810,
+                      year_high: 1099,
+                      current_PE_ratio: 37.82,
+                      market_cap: 22474,
+                      book_value: 28.32,
+                      description: "Colgate is in the Personal Care sector. The current market capitalisation stands at Rs 22,474.17 crore.",
+                      price_to_book_value: 29.18)
+
+    visit "http://localhost:3000/shares/#{share1.id}"
+		
+		expect(page).to have_text(share1.name)
+		expect(page).to have_text(share1.book_value)
+    expect(page).to have_text(share1.current_price)
+    expect(page).to have_text(share1.current_PE_ratio)
+    expect(page).to have_text(share1.market_cap)
+    expect(page).to have_text(share1.price_to_book_value)
+    expect(page).to have_text(share1.description)
+    expect(page).not_to have_text(share2.name)
+	end
+end
