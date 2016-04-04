4fb62831b9a24ff196c8dde9809a860547f59997
SharvariNagesh <sharvari.nagesh@gmail.com>
2016-04-04 22:00:25 +1000

    Adding delay to the spec

--- spec/features/pull_new_share_spec.rb
+++ spec/features/pull_new_share_spec.rb
@@ -1,17 +1,25 @@
 require "spec_helper"
 require "rails_helper"
 
+
+
 describe "Pulling a new share" do
+
+
 	it "Pulls a new share from moneycontrol website url entered" do
 		
     	visit shares_url
     	click_link "Pull Shares"
-    	expect(current_path).to eq(pull_share_path)
+    	expect(current_path).to eq(new_share_path)
 
-    	fill_in('url', :with=>'http://www.moneycontrol.com/india/stockpricequote/food-processing/nestleindia/NI')
+    	fill_in('Url', :with=>'http://www.moneycontrol.com/india/stockpricequote/food-processing/nestleindia/NI')
 
-    	click_button('Pull Share')
-    	expect(current_path).to eq(share_path)
+    	click_button('Pull the data')
+
+        puts "sleeping..."
+        sleep(1.minutes)
+        puts "waking..."
+    	expect(current_path).to eq(shares_path)
     	expect(page).to have_text("Nestle")
 
     end
