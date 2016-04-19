9841c939fe88a08c54e37b69dc06c3958b6b258d
SharvariNagesh <sharvari.nagesh@gmail.com>
2016-04-19 14:57:50 +1000

    Spec to display sectorwise shares

---
+++ spec/features/sector_share_spec.rb
@@ -0,0 +1,20 @@
+require "spec_helper"
+require "rails_helper"
+
+describe "Vieweing the sector of shares" do
+	it "lists all the shares in the sector" do
+		
+		visit shares_url
+	    click_link "Pull Shares"
+	    expect(current_path).to eq(new_share_path)
+
+	    fill_in('Url', :with=>'http://www.moneycontrol.com/india/stockpricequote/food-processing/nestleindia/NI')
+       	click_button('Pull the data')
+
+		expect(page).to have_text("Nestle")
+		click_link "Food Processing"
+		expect(page).to have_text("Shares in Food Processing Sector:")
+		expect(page).to have_text("Nestle")
+	    
+	end
+end
